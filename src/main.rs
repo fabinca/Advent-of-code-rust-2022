07c8ed2cf6fd0c0ec14058797583d352aa972f6a
Carlotta Fabian <73344238+fabinca@users.noreply.github.com>
2022-12-02 17:51:30 +0000

    it's so ugly i'm gonna die

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ use std::path::Path;
 fn main() {
   if let Ok(lines) = read_lines("./src/input.txt") {
         // Consumes the iterator, returns an (Optional) String
-        let mut max_calories = 0;
-        let mut elf_calories = 0;
+        let mut max_calories_vec = vec![0,0,0];
+        let mut elf_calories: i32 = 0;
         for line in lines {
             if let Ok(calorie) = line {
               if calorie.trim().is_empty() {
-                if elf_calories > max_calories {
-                  max_calories = elf_calories;
+                let calorie_to_beat: i32 = *max_calories_vec.iter().min().unwrap_or(&0);
+                if elf_calories > calorie_to_beat {
+                  let index = max_calories_vec.iter().position(|value| *value ==         calorie_to_beat).unwrap_or(4);
+                  if index == 4 {println!("something went wrong")}
+                  max_calories_vec[index] = elf_calories;
                 }
                 elf_calories = 0;
               }
@@ -20,11 +23,18 @@ fn main() {
               }
             }
         }
-      println!("{}", max_calories);
+      let calorie_to_beat: i32 = *max_calories_vec.iter().min().unwrap_or(&0);
+                if elf_calories > calorie_to_beat {
+                  let index = max_calories_vec.iter().position(|value| *value ==         calorie_to_beat).unwrap_or(4);
+                  if index == 4 {println!("something went wrong")}
+                  max_calories_vec[index] = elf_calories;}
+      println!("{:?}", max_calories_vec);
+      println!("{}", max_calories_vec[0] + max_calories_vec[1] + max_calories_vec[2] )
     }
   else {
     println!("File not found")
   }
+   println!("Done")
 }
 
 // The output is wrapped in a Result to allow matching on errors
